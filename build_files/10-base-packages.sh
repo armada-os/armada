0d71017d034dc28ac25bd674993c54445e6007bf
Radical <radical@radical.fun>
2026-08-22 15:02:21 +0200

    feat(packages): add binutils

--- build_files/10-base-packages.sh
+++ build_files/10-base-packages.sh
@@ -59,7 +59,8 @@ dnf5 -y install --setopt=install_weak_deps=False \
     cage \
     wlr-randr \
     distrobox \
-    wl-clipboard
+    wl-clipboard \
+    binutils
 
 curl --connect-timeout 30 --max-time 120 --retry 3 -fsSL \
     -o /etc/yum.repos.d/negativo17-fedora-multimedia.repo \
