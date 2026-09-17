3ded9f978663b7e40ced116da7c0a148d2bb050a
xXJsonDeruloXx <danielhimebauch@gmail.com>
2026-09-17 12:51:27 -0400

    fix: show charging ETA in Steam Game Mode

--- build_files/40-vendor-system-files.sh
+++ build_files/40-vendor-system-files.sh
@@ -103,6 +103,7 @@ systemctl enable armada-powerd.service
 systemctl enable armada-control.service
 systemctl enable armada-steamos-manager.service
 systemctl --global enable armada-steamos-manager.service
+systemctl --global enable armada-steam-charging-eta.service
 systemctl enable armada-bootimg-sync.service
 systemctl enable armada-esp-rename.service
 systemctl enable armada-boot-hotkeys.service
